62c053593ee876c8fcfc6facc06c675258ee082f
Anton <anton.wiblishauser@gmx.de>
2022-10-19 22:28:25 +0200

    update docs

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":headquarters-connector-modrinth:dokkaHtmlPartial/main":[],":headquarters-connector-mojang:dokkaHtmlPartial/main":[],":headquarters-connector-core:dokkaHtmlPartial/main":[]}'
+sourceset_dependencies = '{":headquarters-connector-modrinth:dokkaHtmlPartial/main":[],":headquarters-connector-mojang:dokkaHtmlPartial/main":[],":headquarters-connector-fabric:dokkaHtmlPartial/main":[],":headquarters-connector-core:dokkaHtmlPartial/main":[]}'
---
+++ headquarters-connector-fabric/docs/Module.md
@@ -0,0 +1,13 @@
+# Module headquarters-connector-modrinth
+The fabric-meta v2 http api client
+
+${dependencyNotice}
+
+# Package dev.nyon.headquarters.connector.fabric
+Contains the `FabricConnector`
+
+# Package dev.nyon.headquarters.connector.fabric.models
+Contains the models of the requests
+
+# Package dev.nyon.headquarters.connector.fabric.requests
+Contains the requests
