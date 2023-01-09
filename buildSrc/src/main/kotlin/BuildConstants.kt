8d80c42381af350ccdb4eab1a8fc6144b5c99a41
Anton <anton.wiblishauser@gmx.de>
2023-01-09 20:41:45 +0100

    update ktor version

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -7,7 +7,7 @@ object BuildConstants {
     const val kotlinVersion = "1.7.20"
     const val serializationVersion = "1.4.1"
     const val coroutinesVersion = "1.6.4"
-    const val ktorVersion = "2.1.2"
+    const val ktorVersion = "2.2.2"
     const val dateTimeVersion = "0.4.0"
     const val kotestVersion = "5.5.1"
 }
